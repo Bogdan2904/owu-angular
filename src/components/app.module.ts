9718773b8787719010b96f918e814b1e0e64ca12
bodan <bbolyubash@icloud.com>
2020-07-22 14:13:16 +0300

    lesson 6 repeat

--- src/components/app.module.ts
+++ src/components/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {IUser} from '../../interfaces/user.inteface';
 
 
 @Component({
@@ -7,7 +9,33 @@ import {Component} from '@angular/core';
   styleUrls: [`./app.component.css`]
 })
 export class AppComponent {
+  form: FormGroup;
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  user: IUser;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = new FormGroup({
+      email: this.email,
+      password: new FormControl('', [Validators.minLength(5)]),
+      confirm: new FormControl('', [Validators.required])
+    }, this.passValidator.bind(this));
+    // this.form = formBuilder.group({
+    //   email: ['', [Validators.required, Validators.email]],
+    //   password: '',
+    //   confirm: ['', Validators.required]
+    // });
+  }
 
-  constructor() {
+  save(form: FormGroup): void {
+    this.user = form.getRawValue();
+    console.log(this.user.email);
+    console.log(this.user);
+  }
+
+  passValidator(form: FormGroup): null | object{
+    const {value: password} = form.controls.password;
+    const {value: confirm} = form.controls.confirm;
+    return password === confirm ? null : {passwordError: true};
   }
 }
+
+
